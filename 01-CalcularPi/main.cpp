ca8889460a3792a84bc0eb493448adcd8669b5c9
laguirresanchez <laguirresanchez@frba.utn.edu.ar>
2023-05-21 23:33:13 -0300

    Update main.cpp

--- 01-CalcularPi/main.cpp
+++ 01-CalcularPi/main.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+using namespace std;
 
+int main(){
+    double pi = 0.0;
+    int signo = 1;
+    int iteraciones = 0;
+
+    while ((pi - 3.141592) < 0)
+    {
+        pi += signo * (1.0 / ((2*iteraciones) +1));
+        signo *= -1;
+        iteraciones++;
+
+    }
+    
+    cout << "El número pi con sus primeros 6 decimales es: " << pi << endl;
+    return 0;
+}
